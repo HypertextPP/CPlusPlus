688b979fea3571f182fe8ed93034053bb234a125
HypertextPP <devbrad2001@gmail.com>
2018-03-25 20:42:21 +0100

    FizzBuzz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int FizzBuzz() {
+    for(int i = 1; i <= 100; i++) {
+        std::string out;
+
+        out += (i % 3 == 0) ? "Fizz" : "";
+        out += (i % 5 == 0) ? "Buzz" : "";
+
+        std::cout << (out.length() == 0 ? std::to_string(i) : out) << std::endl;
+    }
+
     return 0;
+}
+
+int main() {
+    return FizzBuzz();
 }
